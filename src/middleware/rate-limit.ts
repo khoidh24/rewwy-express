eabfb93d4dfd146f04de33f289ce38ae99813b06
khoidh24 <zenorn24@gmail.com>
2026-03-18 17:45:22 +0700

    fix: increase rate limit

--- src/middleware/rate-limit.ts
+++ src/middleware/rate-limit.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimitMiddleware = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 20,
+  limit: 1000,
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false,
   ipv6Subnet: 56,
